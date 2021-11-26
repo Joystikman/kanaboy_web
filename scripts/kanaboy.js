96cc951df7b6f2f06f53db864f6d2988e1fc2710
Maxime Guiraud <maxime.guiraud28@gmail.com>
2021-11-26 12:41:30 +0100

    Test every hiragana :
    + add : full hiragana dictionary
    + add : disable auto zoom on mobile

--- scripts/kanaboy.js
+++ scripts/kanaboy.js
@@ -25,22 +25,46 @@ function shuffle(array) {
 
 //dict
 const hiragana_dict = [
-    {key:"a",value:"あ"},
-    {key:"i",value:"い"},
-    {key:"u",value:"う"},
-    {key:"e",value:"え"},
-    {key:"o",value:"お"},
-    {key:"ka",value:"か"},
-    {key:"ki",value:"き"},
-    {key:"ku",value:"く"},
-    {key:"ke",value:"け"},
-    {key:"ko",value:"こ"},
+    {key:"a",value:"あ"},{key:"i",value:"い"},{key:"u",value:"う"},{key:"e",value:"え"},{key:"o",value:"お"},
+    {key:"ka",value:"か"},{key:"ki",value:"き"},{key:"ku",value:"く"},{key:"ke",value:"け"},{key:"ko",value:"こ"},
+    {key:"sa",value:"さ"},{key:"shi",value:"し"},{key:"su",value:"す"},{key:"se",value:"せ"},{key:"so",value:"そ"},
+    {key:"ta",value:"た"},{key:"chi",value:"ち"},{key:"tsu",value:"つ"},{key:"te",value:"て"},{key:"to",value:"と"},
+    {key:"na",value:"な"},{key:"ni",value:"に"},{key:"nu",value:"ぬ"},{key:"ne",value:"ね"},{key:"no",value:"の"},
+    {key:"ha",value:"は"},{key:"hi",value:"ひ"},{key:"fu",value:"ふ"},{key:"he",value:"へ"},{key:"ho",value:"ほ"},
+    {key:"ma",value:"ま"},{key:"mi",value:"み"},{key:"mu",value:"む"},{key:"me",value:"め"},{key:"mo",value:"も"},
+    {key:"ya",value:"や"},{key:"yu",value:"ゆ"},{key:"yo",value:"よ"},
+    {key:"ra",value:"ら"},{key:"ri",value:"り"},{key:"ru",value:"る"},{key:"re",value:"れ"},{key:"ro",value:"ろ"},
+    {key:"wa",value:"わ"},{key:"o",value:"を"},
+    {key:"n",value:"ん"},
+];
+const hiragana_complet_dict = [
+    {key:"a",value:"あ"},{key:"i",value:"い"},{key:"u",value:"う"},{key:"e",value:"え"},{key:"o",value:"お"},
+    {key:"ka",value:"か"},{key:"ki",value:"き"},{key:"ku",value:"く"},{key:"ke",value:"け"},{key:"ko",value:"こ"},
+    {key:"sa",value:"さ"},{key:"shi",value:"し"},{key:"su",value:"す"},{key:"se",value:"せ"},{key:"so",value:"そ"},
+    {key:"ta",value:"た"},{key:"chi",value:"ち"},{key:"tsu",value:"つ"},{key:"te",value:"て"},{key:"to",value:"と"},
+    {key:"na",value:"な"},{key:"ni",value:"に"},{key:"nu",value:"ぬ"},{key:"ne",value:"ね"},{key:"no",value:"の"},
+    {key:"ha",value:"は"},{key:"hi",value:"ひ"},{key:"fu",value:"ふ"},{key:"he",value:"へ"},{key:"ho",value:"ほ"},
+    {key:"ma",value:"ま"},{key:"mi",value:"み"},{key:"mu",value:"む"},{key:"me",value:"め"},{key:"mo",value:"も"},
+    {key:"ya",value:"や"},{key:"yu",value:"ゆ"},{key:"yo",value:"よ"},
+    {key:"ra",value:"ら"},{key:"ri",value:"り"},{key:"ru",value:"る"},{key:"re",value:"れ"},{key:"ro",value:"ろ"},
+    {key:"wa",value:"わ"},{key:"o",value:"を"},
+    {key:"n",value:"ん"},
+    {key:"ga",value:"が"},{key:"gi",value:"ぎ"},{key:"gu",value:"ぐ"},{key:"ge",value:"げ"},{key:"go",value:"ご"},
+    {key:"za",value:"ざ"},{key:"ji",value:"じ"},{key:"zu",value:"ず"},{key:"ze",value:"ぜ"},{key:"zo",value:"ぞ"},
+    {key:"da",value:"だ"},{key:"ji",value:"ぢ"},{key:"zu",value:"づ"},{key:"de",value:"で"},{key:"do",value:"ど"},
+    {key:"ba",value:"ば"},{key:"bi",value:"び"},{key:"bu",value:"ぶ"},{key:"be",value:"べ"},{key:"bo",value:"ぼ"},
+    {key:"pa",value:"ぱ"},{key:"pi",value:"ぴ"},{key:"pu",value:"ぷ"},{key:"pe",value:"ぺ"},{key:"po",value:"ぽ"},
+    {key:"kya",value:"きゃ"},{key:"kyu",value:"きゅ"},{key:"kyo",value:"きょ"},
+    {key:"sha",value:"しゃ"},{key:"shu",value:"しゅ"},{key:"sho",value:"しょ"},
+    {key:"cha",value:"ちゃ"},{key:"chu",value:"ちゅ"},{key:"cho",value:"ちょ"},
+    {key:"nya",value:"にゃ"},{key:"nyu",value:"にゅ"},{key:"nyo",value:"にょ"},
+    {key:"hya",value:"ひゃ"},{key:"hyu",value:"ひゅ"},{key:"hyo",value:"ひょ"},
+    {key:"mya",value:"みゃ"},{key:"myu",value:"みゅ"},{key:"myo",value:"みょ"},
+    {key:"rya",value:"りあ"},{key:"ryu",value:"りゅ"},{key:"ryo",value:"りょ"},
 ];
 
 //init HTML element
 var gamescreen = document.getElementById("gamescreen");
-/* var katakana_menu = document.getElementById("katakana_menu");
-var katakana_menu = document.getElementById("katakana_menu"); */
 
 //variables
 var testLength = 0;
@@ -65,17 +89,27 @@ function setMenu(){
     let list = createNode("ul");
     list.id = "kana_list";
     list.className = "menu_list";
+    /* menu hiragana (46) */
     let hiragana = createNode("li");
     hiragana.id = "hiragana_menu";
+    hiragana.className = "menu";
+    hiragana.innerHTML = "Hiragana (46 kanas)";
+    hiragana.onclick = displayTestLengthMenu;
+    /* menu hiragana (complet) */
+    let hiragana_complet = createNode("li");
+    hiragana_complet.id = "hiragana_complet_menu";
+    hiragana_complet.className = "menu";
+    hiragana_complet.innerHTML = "Hiragana (104 kanas)";
+    hiragana_complet.onclick = displayTestLengthMenu;
+    /* menu katakana (46) */
     let katakana = createNode("li");
     katakana.id = "katakana_menu";
-    hiragana.innerHTML = "Hiragana";
-    hiragana.className = "menu";
-    katakana.innerHTML = "Katakana";
     katakana.className = "menu";
-    hiragana.onclick = displayTestLengthMenu;
+    katakana.innerHTML = "Katakana";
     katakana.onclick = displayTestLengthMenu;
+    /* add to menu list */
     append(list,hiragana);
+    append(list,hiragana_complet);
     append(list,katakana);
     append(gameinput,list);
     append(gamescreen,gameinput);
@@ -83,6 +117,7 @@ function setMenu(){
 
 function displayTestLengthMenu(){
     if (this.id === "hiragana_menu"){kanaTested = "hiragana"}
+    if (this.id === "hiragana_complet_menu"){kanaTested = "hiragana_complet"}
     if (this.id === "katakana_menu"){kanaTested = "katakana"}
     console.log("click");
     gamescreen.innerHTML = "";
@@ -124,6 +159,14 @@ function initQuizz(){
     questions = [];
     if (kanaTested === "hiragana"){
         tempList = JSON.parse(JSON.stringify(hiragana_dict));
+        shuffle(tempList);
+        console.log(tempList);
+        for (let index = 0; index < testLength; index++) {
+            questions.push(tempList.pop());
+        }
+    }
+    if (kanaTested === "hiragana_complet"){
+        tempList = JSON.parse(JSON.stringify(hiragana_complet_dict));
         shuffle(tempList);
         console.log(tempList);
         for (let index = 0; index < testLength; index++) {
@@ -211,6 +254,12 @@ function testEnd(){
     console.log("correct answer :"+correctAnswer);
     if (testLength == 5) {
         testStats.serie5.push({date:new Date(), score: correctAnswer});
+    }
+    if (testLength == 10) {
+        testStats.serie10.push({date:new Date(), score: correctAnswer});
+    }
+    if (testLength == 20) {
+        testStats.serie20.push({date:new Date(), score: correctAnswer});
     }
     console.log(testStats);
     gamescreen.innerHTML = "";
